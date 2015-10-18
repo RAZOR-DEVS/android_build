2705bb0555ab5bf709bf4a6e1c9a55929a12ae7f
vibhu0009 <cvibhu0009@gmail.com>
2015-10-18 12:20:02 +0200

    make razor

--- envsetup.sh
+++ envsetup.sh
@@ -527,7 +527,7 @@ function brunch()
 {
     breakfast $*
     if [ $? -eq 0 ]; then
-        mka bacon
+        mka razor
     else
         echo "No such item in brunch menu. Try 'breakfast'"
         return 1
@@ -2006,7 +2006,7 @@ function cmka() {
     if [ ! -z "$1" ]; then
         for i in "$@"; do
             case $i in
-                bacon|otapackage|systemimage)
+                razor|otapackage|systemimage)
                     mka installclean
                     mka $i
                     ;;
